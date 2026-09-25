ff3d542e6b8c478cc9240f53eff91eec81ef46e5
JosueNeo <132624093+elmerneo@users.noreply.github.com>
2026-09-24 18:22:28 -0600

    Add prestamo.routes to server.js

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.get("/", (req, res) => {
 require("./app/routes/cliente.routes")(app);
 require("./app/routes/producto.routes")(app);
 require("./app/routes/proveedor.routes")(app);
+require("./app/routes/prestamo.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8081;
